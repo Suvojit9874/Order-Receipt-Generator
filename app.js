057e3e5b8e59dc21889dacd743b81ca2c1bbacf3
Suvojit Chatterjee <suvojitchatterjee84@gmail.com>
2025-03-08 15:33:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -148,4 +148,4 @@ app.post("/generate-receipt", (req, res) => {
   }, 1000);
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app.listen(8000, () => console.log("Server running on http://localhost:3000"));
